e6fce6f9821cb748b00ee2d8c47a41a6b1dda620
Koreh Andrei <koreh.andrei20@gmail.com>
2019-09-27 17:44:28 +0300

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+
 import GlobalStyle from './global.styles';
 
 const App = () => (
-  <GlobalStyle />
+  <React.Fragment>
+    <GlobalStyle />
+    <Switch>
+      <Route exact path="/" render={()=> <h1>HomePage</h1>}/>
+    </Switch>
+  </React.Fragment>
 );
 
 export default App;
