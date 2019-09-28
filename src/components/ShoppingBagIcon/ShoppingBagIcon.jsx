3436fd09b8806ab3eaf70f1f8c8175ecd82a78e4
Koreh Andrei <koreh.andrei20@gmail.com>
2019-09-28 09:49:44 +0300

    Added ability to hide/show cart drodown

--- src/components/ShoppingBagIcon/ShoppingBagIcon.jsx
+++ src/components/ShoppingBagIcon/ShoppingBagIcon.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleCartDropdownHidden } from '../../redux/cart/cart.actions';
 
 import {ShoppingBagIconContainer, ShoppingBagIconAsset, ItemCountContainer} from './ShoppingBagIcon.styles';
 
-const ShoppingBagIcon = ({itemCount}) => (
-    <ShoppingBagIconContainer>
+const ShoppingBagIcon = ({toggleCartDropdownHidden, itemCount}) => (
+    <ShoppingBagIconContainer onClick={toggleCartDropdownHidden}>
         <ShoppingBagIconAsset />
         <ItemCountContainer>{itemCount}</ItemCountContainer>
     </ShoppingBagIconContainer>
 );
 
-export default ShoppingBagIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCartDropdownHidden: () => dispatch(toggleCartDropdownHidden())
+});
+
+export default connect(null, mapDispatchToProps)(ShoppingBagIcon);
