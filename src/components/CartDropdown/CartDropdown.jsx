1a1764dfc09f157071a1a72766b1fd024cc6db21
Koreh Andrei <koreh.andrei20@gmail.com>
2019-09-28 10:05:17 +0300

    Added tests for CartDropdown being shown in NavBar

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {CartDropdownContainer, EmptyMessageContainer, CartItemsContainer} from './CartDropdown.styles';
 
-const CartDropdown = () => (
+export const CartDropdown = () => (
     <CartDropdownContainer>
         <CartItemsContainer>
             <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { NavBar } from './NavBar';
+import { CartDropdown } from '../CartDropdown/CartDropdown';
 
 describe('NavBar component', () => {
     let wrapper;
@@ -17,5 +18,19 @@ describe('NavBar component', () => {
 
     it('should render NavBar', () => {
         expect(wrapper).toMatchSnapshot();
+    });
+
+    describe('CartDropdown being shown', () => {
+        it('should not render CartDropdown if its hidden', () => {
+            expect(wrapper.exists(CartDropdown)).toBe(false);
+        });
+
+        it('should render CartDropdow if its not hidden', () => {
+            const newProps = {...mockProps, cartDropdownHidden: false};
+
+            const wrapper = shallow(<NavBar {...newProps}/>)
+
+            expect(wrapper.exists(CartDropdown)).toBe(true);
+        });
     });
 });
